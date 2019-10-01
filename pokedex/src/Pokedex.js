9145818f4927dace98cb6873afa9bf872af3bd06
Rachel Chhay <rachelchhay@rachels-mbp.attlocal.net>
2019-10-01 15:36:03 -0500

    setting up components

---
+++ pokedex/src/Pokedex.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Pokecard from './Pokecard';
+
+const Pokedex = () => {
+    return (
+        <div>
+            <h2>Pokedex is working</h2>
+            <Pokecard />
+        </div>
+    );
+}
+
+export default Pokedex;
