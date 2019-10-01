be2958287c169f37f1035d249a7ba7539657ce9e
Rachel Chhay <rachelchhay@rachels-mbp.attlocal.net>
2019-10-01 16:42:44 -0500

    passing through props

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -5,6 +5,7 @@ import Pokedex from './Pokedex';
 function App() {
   return (
     <div className="App">
+      <h1>Pokedex</h1>
       <Pokedex />
     </div>
   );
--- pokedex/src/Pokedex.js
+++ pokedex/src/Pokedex.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Pokecard from './Pokecard';
 
-const Pokedex = () => {
+const Pokedex = (props) => {    
     return (
         <div>
-            <h2>Pokedex is working</h2>
-            <Pokecard />
+            {
+                props.pokemonArr.map((pokemon) => {
+                    return <Pokecard id={pokemon.id} />
+                    console.log(pokemon);
+                })
+            }
         </div>
     );
+}
+
+Pokedex.defaultProps = {
+    pokemonArr: [
+        {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
+        {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
+        {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
+        {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
+        {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
+        {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
+        {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
+        {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
+    ]
 }
 
 export default Pokedex;
